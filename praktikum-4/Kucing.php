647e0553619167d54dd6af7f1b5f99bd0ce60698
Fadila Adelia <92003518+fadiladelia@users.noreply.github.com>
2022-03-21 14:56:45 +0700

    add praktikum-4

---
+++ praktikum-4/Kucing.php
@@ -0,0 +1,30 @@
+<?php
+//membuat class kucing
+class Kucing{
+    //atribut
+    //visisbility : public, private, etc
+    public $nama;
+    public $umur;
+    public $warna;
+
+    // membuat constructor
+    public function __construct($nama, $umur, $warna){
+        $this->nama = $nama;
+        $this->umur = $umur;
+        $this->warna = $warna;
+    }
+    
+
+    //behavior / method
+    public function makan(){
+        return "{$this->nama} sedang makan ";
+    }
+
+    public function tidur(){
+        return "{$this->nama} sedang tidur";
+    }
+
+    public function lari(){
+        return "{$this->nama} sedang lari";
+    }
+}
---
+++ praktikum-4/index.php
@@ -0,0 +1,17 @@
+<?php
+include_once "Kucing.php";
+
+// membuat objek / instance objek kucing
+$garfield = new Kucing("Garfield", 5, "Oren");
+$bolang = new Kucing("Bolang", 10, "Belang Hitam");
+
+echo $garfield->nama, "<br/>";
+
+echo $garfield->makan(), "<br/>";
+echo $garfield->tidur(), "<br/>";
+echo $garfield->lari(), "<br/>";
+
+echo $bolang->makan(), "<br/>";
+echo $bolang->tidur(), "<br/>";
+echo $bolang->lari();
+?>
